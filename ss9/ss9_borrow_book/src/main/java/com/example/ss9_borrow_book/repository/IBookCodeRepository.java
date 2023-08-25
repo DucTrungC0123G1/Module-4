d7be14bc3edc9a04946b705806081d238cea4f27
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-08-25 14:08:01 +0700

    nop bai ss9

--- ss9/ss9_borrow_book/src/main/java/com/example/ss9_borrow_book/repository/IBookCodeRepository.java
+++ ss9/ss9_borrow_book/src/main/java/com/example/ss9_borrow_book/repository/IBookCodeRepository.java
@@ -4,12 +4,17 @@ import com.example.ss9_borrow_book.model.BookCode;
 import javafx.beans.property.adapter.JavaBeanProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IBookCodeRepository extends JpaRepository<BookCode,Integer> {
 
+
+    @Query(value = "select * from book join book_code on book_code.book_id = book.id where book_code.code =:code",nativeQuery = true)
+    BookCode findBookCodeByCode(@Param("code") int code);
+
     @Transactional
-    @Modifying
-    BookCode findBookCodeByCode(int code);
     void deleteBookByCode(int code);
 }
